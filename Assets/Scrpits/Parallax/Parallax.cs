de369694eef33b20f94eb4b2aa57a05d618f7860
joão Pedro <jao.pedro.beraldo@gmail.com>
2020-04-14 18:16:30 -0300

    Parallax

---
+++ Assets/Scrpits/Parallax/Parallax.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    public Transform Alvo;
+    public float velocidadeRelativa;
+    public float posicaoAntX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (velocidadeRelativa < 1)
+            velocidadeRelativa = 1;
+        Alvo =  GameObject.FindGameObjectWithTag("Player").transform;
+        posicaoAntX = Alvo.position.x;
+
+    }
+
+    void EfeitoParallax()
+    {
+        transform.Translate((Alvo.position.x - posicaoAntX) / velocidadeRelativa, 0, 0);
+        posicaoAntX = Alvo.position.x;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        EfeitoParallax();
+    }
+}
