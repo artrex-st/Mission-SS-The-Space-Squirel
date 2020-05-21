18df085ae40990ae25c96e06e2a99606405af0af
Arthur-St <artrexwow@gmail.com>
2020-05-20 21:45:20 -0300

    Update Add [TEST]
    Pause game + Pause Ui

---
+++ Assets/Scrpits/ScenarioScripts/Puse.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puse : MonoBehaviour
+{
+    public bool isPaused;
+    public Canvas pauseScreen;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        pauseScreen = GetComponentInChildren<Canvas>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPaused)
+        {
+            pauseScreen.enabled = true;
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            pauseScreen.enabled = false;
+            Time.timeScale = 1f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+        }
+    }
+}
